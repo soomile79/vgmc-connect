c4e21ecbfb30b218244a681b4b43c6554a9b1a77
soomile79 <161687547+soomile79@users.noreply.github.com>
2026-01-10 16:35:22 -0800

    Prevent 'process is not defined' errors in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,14 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   // Load env file based on `mode` in the current working directory.
-  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
   const env = loadEnv(mode, (process as any).cwd(), '');
   return {
     plugins: [react()],
     define: {
       // Allow access to process.env.API_KEY in the browser
       'process.env.API_KEY': JSON.stringify(env.API_KEY),
+      // Prevent "process is not defined" errors in some 3rd party libs
+      'process.env': {},
     },
   };
 });
